d38ed4d2927117f29f44c9117cc777723efebddf
Taeun Lee <tulee3474@naver.com>
2023-08-02 18:07:51 +0900

    [FEAT] 포스트맨에서 JSON 파일 전송으로 ai 실행

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ var { localSearchAI, enoughPlace } = require('./local_search_ai.js');
 //const path = require('path');
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-//const userRoute = require('./api/routes/user');
-const router = express.Router();
 const port = Number(process.env.PORT) || 8080;
 
 var http = require('http');
@@ -33,10 +30,10 @@ const selectListInRegion = [
     [0, 1, 1, 0],
 ];
 const accomodationList = [
-    { name: '', selectList: selectList, lat: 35.333, lng: 122.32323, takenTime: 30, category: 4 },
-    { name: '', selectList: selectList, lat: 35.333, lng: 122.32323, takenTime: 30, category: 4 },
-    { name: '', selectList: selectList, lat: 35.333, lng: 122.32323, takenTime: 30, category: 4 },
-    { name: '', selectList: selectList, lat: 34.333, lng: 121.32323, takenTime: 30, category: 4 },
+    { name: '', lat: 35.333, lng: 122.32323, takenTime: 30, category: 4 },
+    { name: '', lat: 35.333, lng: 122.32323, takenTime: 30, category: 4 },
+    { name: '', lat: 35.333, lng: 122.32323, takenTime: 30, category: 4 },
+    { name: '', lat: 34.333, lng: 121.32323, takenTime: 30, category: 4 },
 ];
 const essentialPlaceList = [
     {
@@ -75,99 +72,44 @@ app.listen(port, () => {
     //console.log(`server is listening at localhost:${process.env.PORT}`);
 });
 
-// Get users
-router.get('/', (req, res, next) => {
-    res.status(200).json(users);
-});
+app.use(express.json()); // JSON 데이터 파싱을 위한 미들웨어
 
-app.get('/', async (req, res) => {
+app.get('/ai/data', async (req, res) => {
+    // 클라이언트로부터 전달된 JSON 데이터
+    const requestData = req.query.data;
+
     if (!firstTime) {
         firstTime = true;
         console.log('파비콘땜에 두번 실행됨');
     } else {
-        // 3. 콘솔화면에 로그 시작 부분을 출력
-        console.log('--- log start ---');
+        try {
+            // JSON 데이터 파싱
+            const jsonData = JSON.parse(requestData);
 
-        await localSearchAI({
-            regionList: regionList,
-            accomodationList: accomodationList,
-            selectList: selectList,
-            essentialPlaceList: essentialPlaceList,
-            timeLimitArray: timeLimitArray,
-            nDay: nDay + 1,
-            transit: transit,
-            distanceSensitivity: distanceSensitivity,
-        });
-        //.catch((err) => console.error('멀티스레딩 중에 에러 발생 ' + err));
+            // 파싱된 데이터를 이용하여 처리 로직 수행
 
-        // console.time('prime2');
-        //const primeGeneratorPath = path.join('./test.js');
-        //await test_function_run().catch((err) => console.error('멀티스레딩 중에 에러 발생 ' + err));
+            // 3. 콘솔화면에 로그 시작 부분을 출력
+            console.log('--- log start ---');
 
-        //console.timeEnd('prime2');
-        // 4. 브라우저에서 요청한 주소를 parsing 하여 객체화 후 출력
-        //var parsedUrl = url.parse(request.url);
-        //console.log(parsedUrl);
-        // 5. 객체화된 url 중에 Query String 부분만 따로 객체화 후 출력
-        //var parsedQuery = querystring.parse(parsedUrl.query, '&', '=');
-        //console.log(parsedQuery);
-        // 6. 콘솔화면에 로그 종료 부분을 출력
-        console.log('--- log end ---');
+            await localSearchAI({
+                regionList: jsonData['regionList'],
+                accomodationList: jsonData['accomodationList'],
+                selectList: jsonData['selectList'],
+                essentialPlaceList: jsonData['essentialPlaceList'],
+                timeLimitArray: jsonData['timeLimitArray'],
+                nDay: jsonData['nDay'],
+                transit: jsonData['transit'],
+                distanceSensitivity: jsonData['distanceSensitivity'],
+            });
+            console.log('--- log end ---');
+            firstTime = false;
 
-        // response.writeHead(200, { 'Content-Type': 'text/html' });
-        // response.end('Hello node.js!!');
-        // response.writeHead(200, { 'Content-Type': 'text/html' });
-        //response.end('var1 :  ' + parsedQuery.var1);
-        firstTime = false;
-    }
-    res.json({
-        success: true,
-    });
-});
-/*
-var server = http.createServer(async function (request, response) {
-    if (!firstTime) {
-        firstTime = true;
-        console.log('파비콘땜에 두번 실행됨');
-    } else {
-        // 3. 콘솔화면에 로그 시작 부분을 출력
-        console.log('--- log start ---');
-
-        await localSearchAI({
-            regionList: regionList,
-            accomodationList: accomodationList,
-            selectList: selectList,
-            essentialPlaceList: essentialPlaceList,
-            timeLimitArray: timeLimitArray,
-            nDay: nDay + 1,
-            transit: transit,
-            distanceSensitivity: distanceSensitivity,
-        });
-        //.catch((err) => console.error('멀티스레딩 중에 에러 발생 ' + err));
-
-        // console.time('prime2');
-        //const primeGeneratorPath = path.join('./test.js');
-        //await test_function_run().catch((err) => console.error('멀티스레딩 중에 에러 발생 ' + err));
-
-        //console.timeEnd('prime2');
-        // 4. 브라우저에서 요청한 주소를 parsing 하여 객체화 후 출력
-        var parsedUrl = url.parse(request.url);
-        //console.log(parsedUrl);
-        // 5. 객체화된 url 중에 Query String 부분만 따로 객체화 후 출력
-        var parsedQuery = querystring.parse(parsedUrl.query, '&', '=');
-        //console.log(parsedQuery);
-        // 6. 콘솔화면에 로그 종료 부분을 출력
-        console.log('--- log end ---');
-
-        // response.writeHead(200, { 'Content-Type': 'text/html' });
-        // response.end('Hello node.js!!');
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.end('var1 :  ' + parsedQuery.var1);
-        firstTime = false;
+            // 이 예제에서는 그대로 JSON 데이터를 응답으로 보내줍니다.
+            res.json({ success: true, data: jsonData });
+            //res.send('ai success' + jsonData);
+        } catch (error) {
+            // JSON 파싱 에러 처리
+            res.status(400).json({ success: false, error: 'Invalid JSON data' });
+        }
     }
 });
-
-server.listen(8080, function () {
-    console.log('Server is running...');
-});
-*/
