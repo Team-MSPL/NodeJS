b21f478ec24501be458ec329549930a4e4fe7c5f
Taeun Lee <tulee3474@naver.com>
2024-09-11 13:37:10 +0900

    [FEAT] 장소 위도 경도 좌표 읽어오기

---
+++ src/routes/google_geo_place_api.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+require('dotenv').config();
+
+const axiosGoogle = axios.create({
+    baseURL: 'https://maps.googleapis.com/maps/api',
+    headers: { 'content-type': 'application/json' },
+});
+
+async function getPlaceGeo(place) {
+    let placeGeo;
+
+    const response = await axiosGoogle.get(
+        `/place/findplacefromtext/json?input=${place.region}${place.name}&inputtype=textquery&fields=formatted_address%2Cname%2Cgeometry&key=${process.env.GOOGLE_API_KEY}`
+    );
+
+    if (response.statusCode < 200 || response.statusCode > 400) {
+        placeGeo = { lat: 0.0, lng: 0.0 }; // Error 반환
+    } else if (response.data.status === 'ZERO_RESULTS') {
+        placeGeo = { lat: 0.0, lng: 0.0 }; // Error 반환
+    } else {
+        placeGeo = response.candidates[0]?.geometry?.location;
+    }
+    return placeGeo;
+}
+
+async function googleGeoApi(place) {
+    let placeGeoInfo = await getPlaceGeo(place);
+
+    return placeGeoInfo;
+}
+
+module.exports.googleGeoApi = googleGeoApi;
